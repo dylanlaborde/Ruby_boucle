d30414ff825327d85d3d74ff5807c154adacafe5
dylanlaborde <dylan4047@gmail.com>
2021-04-14 17:04:33 +0200

    rajout def say_hello

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,11 +1,17 @@
 #Vald a besoin de toi pour l'écriture de sa prochaine chanson à succès : il lui faudrait un programme qui dit bonjour.
+#Code une méthode say_hello qui va dire bonjour quand on l'exécute
+
+
 
 def init()
-    puts "Bonjour , dit bonjour"
+    say_hello
+end
+
+def say_hello
+    puts "Bonjour"
     user_input = gets.chomp.to_s
     politesse(user_input)
 end
-
 
 def politesse(a)
 
