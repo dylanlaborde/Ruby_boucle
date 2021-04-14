ada02fb5f33dcfc601c4efabf3a8df817ccaf03f
dylanlaborde <dylan4047@gmail.com>
2021-04-14 17:25:44 +0200

    le programe te dit bonjour

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -4,23 +4,43 @@
 
 
 def init()
-    say_hello
+    say_hello(nil)
 end
 
-def say_hello
-    puts "Bonjour"
-    user_input = gets.chomp.to_s
-    politesse(user_input)
+def say_hello(first_name)
+
+    if first_name == nil
+        puts "Bonjour"
+        user_input = gets.chomp.to_s
+        politesse(user_input)
+    elsif first_name != nil
+        politesse(first_name)
+    else
+        politesse
+    end
 end
 
 def politesse(a)
-
+name = ""
     if a.upcase == "BONJOUR" || a.downcase == "bonjour"
-        puts "bonjour mon ami"
+        
+        puts "quel est ton prenom ?"
+        name = get_first_name()
+    elsif a.upcase != "BONJOUR" || a.downcase != "bonjour"
+        say_hello(a)
     else
         puts "jvais te niquer ta mere"
     end
 
+    if name != ""
+        puts "Bonjour, #{name} !"
+    end
+
+end
+
+def get_first_name()
+    first_name = gets.chomp.to_s
+    return first_name
 end
 
 init()
